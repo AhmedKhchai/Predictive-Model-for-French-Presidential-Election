9eb6d1bf38592c7d85496422ce4f3aeaaee2deef
AhmedKhchai <a.khchai@gmail.com>
2023-08-29 11:26:48 +0200

    refactored random_forest_model_0.72

--- app/model/prediction_model.py
+++ app/model/prediction_model.py
@@ -1,13 +1,65 @@
+import os
 import streamlit as st
 import pandas as pd
+import joblib
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, classification_report
 
 
+def train_and_evaluate_model(X_train, X_val, y_train, y_val):
+    # Initialize the Random Forest Classifier
+    rf_classifier = RandomForestClassifier(random_state=42)
+    rf_classifier.fit(X_train, y_train)
+
+    # Evaluate the model
+    y_val_pred = rf_classifier.predict(X_val)
+    accuracy = accuracy_score(y_val, y_val_pred)
+    classification_rep = classification_report(y_val, y_val_pred)
+
+    # Return trained model and evaluation metrics
+    return rf_classifier, accuracy, classification_rep
+
+
 def main():
     st.title("Prediction Model - Presidential Election")
 
+    # Data Preprocessing
+    final_data = load_preprocess_data()
+
+    # Prepare features and labels for model training
+    X = final_data.drop('Winner_2017', axis=1)
+    y = final_data['Winner_2017']
+    X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    # Fetch saved
+    if not os.path.exists('saved_models'):
+        os.makedirs('saved_models')
+    model_files = os.listdir('saved_models/')
+    selected_model_file = st.selectbox('Select a saved model:', model_files)
+
+    # Display details about the selected model
+    if selected_model_file:
+        loaded_model = joblib.load(f'saved_models/{selected_model_file}')
+        st.write(f"Details for model {selected_model_file}")
+        st.write(f"Number of Estimators: {loaded_model.n_estimators}")
+        st.write(f"Criterion: {loaded_model.criterion}")
+        st.write(f"Max Depth: {loaded_model.max_depth}")
+
+    # Button to train and save the model
+    if st.button('Train Model and Save'):
+        rf_classifier, accuracy, classification_rep = train_and_evaluate_model(X_train, X_val, y_train, y_val)
+
+        # Save the model to a file
+        joblib.dump(rf_classifier, f'saved_models/random_forest_model_{accuracy}.joblib')
+
+        # Display the model information
+        st.write(f"Model Accuracy: {accuracy}")
+        st.write("Classification Report:")
+        st.text(classification_rep)
+
+
+def load_preprocess_data():
     # Load datasets
     election_data_2017 = pd.read_csv('datasets/election-dataset-2017-cleaned.csv')
     employment_data = pd.read_csv('datasets/merged-employment-dataset.csv')
@@ -55,24 +107,7 @@ def main():
 
     # Create a new DataFrame with only the selected features and target variable
     final_data_selected_features = final_merged_data[selected_features]
-
-    # Random Forest Model
-    X = final_data_selected_features.drop('Winner_2017', axis=1)
-    y = final_data_selected_features['Winner_2017']
-
-    X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=42)
-
-    # Initialize the Random Forest Classifier
-    rf_classifier = RandomForestClassifier(random_state=42)
-    rf_classifier.fit(X_train, y_train)
-
-    y_val_pred = rf_classifier.predict(X_val)
-    accuracy = accuracy_score(y_val, y_val_pred)
-    classification_rep = classification_report(y_val, y_val_pred)
-
-    st.write(f"Model Accuracy: {accuracy}")
-    st.write("Classification Report:")
-    st.text(classification_rep)
+    return final_data_selected_features
 
 
 if __name__ == "__main__":
